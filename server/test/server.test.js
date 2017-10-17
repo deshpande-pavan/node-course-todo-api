da113d7c1e9ad6e25e7e7b092c2fecf5d8fc0133
Pavan <pavandeshpande60@gmail.com>
2017-10-17 12:25:19 +0530

    Test POST /todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -36,4 +36,21 @@ describe('POST /todos', () => {
                 })
             });
     })
+    it('should not create todos with invalid data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e)=>{
+                    done(e);
+                })
+            })
+    })
 });
