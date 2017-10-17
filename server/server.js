bcea473fb5a409f18d8c16074706ed5fd2449ea3
Pavan <pavandeshpande60@gmail.com>
2017-10-17 12:45:26 +0530

    Add GeT /todos route

--- server/server.js
+++ server/server.js
@@ -14,15 +14,23 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    })
 });
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports={app};
+module.exports = { app };
