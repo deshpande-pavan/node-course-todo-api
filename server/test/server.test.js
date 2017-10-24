827e17a9473d6af446f5509c23091ac717958868
Pavan <pavandeshpande60@gmail.com>
2017-10-24 17:59:28 +0530

    DELETE test by /todos/:id route (1 error Cannot read property '_id' of undefined)

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,21 +1,21 @@
 const expect = require('expect');
 const request = require('supertest');
-const { ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
 var { app } = require('./../server');
 var { Todo } = require('./../models/todo');
 
-var todos= [{
-    _id:new ObjectId(),
-    text:"First test"
+var todos = [{
+    _id: new ObjectId(),
+    text: "First test"
 }, {
-    _id:new ObjectId(),
-    text:"Second test"
+    _id: new ObjectId(),
+    text: "Second test"
 }];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
         return Todo.insertMany(todos);
-    }).then(()=>{
+    }).then(() => {
         done();
     })
 });
@@ -36,7 +36,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find({text}).then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -57,45 +57,83 @@ describe('POST /todos', () => {
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(2);
                     done();
-                }).catch((e)=>{
+                }).catch((e) => {
                     done(e);
                 })
             })
     })
 });
 
-describe('GET /todos',()=>{
-    it('should get all todos',(done)=>{
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
         request(app)
-        .get('/todos')
-        .expect(200)
-        .expect((res)=>{
-            expect(res.body.todos.length).toBe(2);
-        })
-        .end(done);
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     })
 })
 
-describe('GET /todos/:id',()=>{
-    it('should return todo doc',(done)=>{
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
         request(app)
             .get(`/todos/${todos[0]._id.toHexString()}`)
             .expect(200)
-            .expect((res)=>{
+            .expect((res) => {
                 expect(res.body.todos.text).toBe(todos[0].text)
             })
             .end(done);
     });
-    it('should return 404 if todo not found',(done)=>{
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectId().toHexString();
         request(app)
-            .get(`/todos/${new ObjectId().toHexString()}`)
+            .get(`/todos/${hexId}`)
             .expect(404)
             .end(done);
     });
-    it('should return 404 for non object ids',(done)=>{
+    it('should return 404 for non object ids', (done) => {
         request(app)
             .get(`/todos/${123}`)
             .expect(404)
             .end(done);
-    })
-})
+    });
+});
+
+describe('DELETE /todo/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectId().toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non object ids', (done) => {
+        request(app)
+            .get(`/todos/${123}`)
+            .expect(404)
+            .end(done);
+    });
+});
