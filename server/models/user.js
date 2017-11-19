fb1d9cf2a721f66eba201a2459673a4b58ca6416
Pavan <pavandeshpande60@gmail.com>
2017-11-19 09:27:14 +0530

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -52,12 +52,12 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
-UserSchema.statics.findByToken= function(token){
+UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
-    try{
-        decoded= jwt.verify(token,'abc123');
-    } catch(e){
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
         // return new Promise((resolve,reject)=>{
         //     reject();
         // });
@@ -65,22 +65,41 @@ UserSchema.statics.findByToken= function(token){
     };
 
     return User.findOne({
-        '_id':decoded._id,
-        'tokens.token':token,
-        'tokens.access':'auth'
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     })
-}; 
+};
 
-UserSchema.pre('save',function(next){
-    var user =this;
-    if(user.isModified('password')){
-        bcrypt.genSalt(10,(err,salt)=>{
-            bcrypt.hash(user.password,salt,(err,hash)=>{
-                user.password=hash;
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                }
+                else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
                 next();
             });
         });
-    }else{
+    } else {
         next();
     }
 });
