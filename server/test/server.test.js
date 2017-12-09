f9b88b04404d54ed3638f268e1d6ee458c93807b
Pavan <pavandeshpande60@gmail.com>
2017-12-09 17:25:47 +0530

    Added async-await to todo api

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -339,7 +339,7 @@ describe('POST /users/login', () => {
   });
 });
 
-describe('/users/me/token', () => {
+describe('DELETE /users/me/token', () => {
   it('should remove auth token on logout', (done) => {
     request(app)
       .delete('/users/me/token')
